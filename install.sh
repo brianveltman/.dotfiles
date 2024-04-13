c2c0e761f9501179ccb1e2f7714b498f3d926942
Brian Veltman <2551674+brianveltman@users.noreply.github.com>
2024-04-14 01:06:07 +0200

    Split macOS and Linux config

--- install.sh
+++ install.sh
@@ -1,35 +1,12 @@
 #!/usr/bin/env bash
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Start installing all applications and binaries
-echo "Setting up your personal settings..."
-
-# Update Homebrew recipes and disable analytics from being send
-brew analytics off
-brew update
-
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
-
-# Make ZSH the default shell environment
-# chsh -s $(which zsh)
-
-# Install global Yarn packages
-# yarn global add create-react-app yo
-
-# Create a Code directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
-# mkdir $HOME/Sites
-
-# Set macOS preferences
-# We will run this last because this will reload the shell
-# source macOS/.macOS
-# source macOS/.dock
-
-# Install extra stuff
-# if [ -d "$EXTRA_DIR" -a -f "$EXTRA_DIR/install.sh" ]; then
-#   . "$EXTRA_DIR/install.sh"
-# fi
+# determine if we're running on macOS or Linux
+if [ "$(uname)" == "Darwin" ]; then
+  export OS="macOS"
+  # execute macos.sh
+  . macOS.sh
+else
+  export OS="linux"
+  # execute linux.sh
+  . Linux.sh
+fi
